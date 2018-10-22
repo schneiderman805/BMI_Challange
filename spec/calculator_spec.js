2bdf1f3e83ea082ab0cc00a17c02f7f245af7e4c
William Schneiderman <william.m.schneiderman@gmail.com>
2018-10-22 21:14:09 +0200

    changes made with thomas

--- spec/calculator_spec.js
+++ spec/calculator_spec.js
@@ -6,16 +6,16 @@ describe("BMICalculator", function() {
 
   beforeEach(function() {
     person = new Person({weight: 90, height: 186});
-    calculator = new BMICalculator();
+    bmi_calculator = new BMICalculator();
   });
 
   it("calculates BMI for a person using metric method", function() {
-    calculator.metric_bmi(person);
+    bmi_calculator.metric_bmi(person);
     expect(person.bmiValue).to.equal(26.01);
   });
 
   it("sets BMI message for a person using metric method", () => {
-    calculator.metric_bmi(person);
+    bmi_calculator.metric_bmi(person);
     expect(person.bmiMessage).to.equal('Overweight');
 });  
 
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -6,37 +6,9 @@ BMICalculator.prototype.metric_bmi = function(obj) {
     if (weight > 0 && height > 0) {
       var finalBmi = weight / (height / 100 * height / 100);
       obj.bmiValue =  parseFloat(finalBmi.toFixed(2));
+      setBMIMessage(obj);
     }
 
-  function setBMIMessage (obj){
-    if (obj.bmiValue < 18.5) {
-<<<<<<< HEAD
-    obj.bmiMessage = "Underweight"
-    }
-    if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
-    obj.bmiMessage = "Healthy"
-    }
-    if (obj.bmiValue > 25 && obj.bmiValue < 30) {
-    obj.bmiMessage = "Overweight"
-    }
-    if (obj.bmiValue > 30) {
-    obj.bmiMessage = "Obese"
-    }
-  };
-};
-=======
-        obj.bmiMessage = "Underweight"
-    }
-    if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
-        obj.bmiMessage = "Healthy"
-    }
-    if (obj.bmiValue > 25 && obj.bmiValue < 30) {
-        obj.bmiMessage = "Overweight"
-    }
-    if (obj.bmiValue > 30) {
-        obj.bmiMessage = "Obese"
-    }
-  }
 }; 
  
 BMICalculator.prototype.imperial_bmi = function(obj) {
@@ -48,4 +20,17 @@ BMICalculator.prototype.imperial_bmi = function(obj) {
         setBMIMessage(obj);
   }
 };
->>>>>>> 9938fdf6a19e57e3c8ff226a7b61576022861dca
+function setBMIMessage (obj){
+  if (obj.bmiValue < 18.5) {
+    obj.bmiMessage = "Underweight"
+  }
+  if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
+    obj.bmiMessage = "Healthy"
+  }
+  if (obj.bmiValue > 25 && obj.bmiValue < 30) {
+    obj.bmiMessage = "Overweight"
+  }
+  if (obj.bmiValue > 30) {
+    obj.bmiMessage = "Obese"
+  }
+}
